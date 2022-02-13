205835d6cb15dd9ebdbbb446fdb02ad527c9df6f
Nathan Arvan <nathannarvan@gmail.com>
2022-02-13 13:03:40 -0600

    updating grid

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -7,26 +7,25 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
-  @Input() xDimension = 10; 
-  @Input() yDimension = 10; 
+  @Input() public xDimension = 10; 
+  @Input() public yDimension = 10; 
 
   // public columns: any[] = [];
   // public rows: any[] = [];
-  public columns = [1,2,3,4,5,6,7,8,9,10];
-  public rows = [1,2,3,4,5,6,7,8,9,10];
+  public columns: any[] = [];
+  public rows: any[] = [];
   public loading = true;
 
   constructor() { }
 
   ngOnInit(): void {
-    // for (let i =0; i > this.xDimension; i++) {
-    //   this.columns = [...this.columns, {test: 'dummyData'}]
-    // }
-    // for (let i =0; i > this.yDimension; i++) {
-    //   this.rows = [...this.rows, {test: 'dummyData'}]
-    // }
-    console.log(this.columns, this.rows)
+    this.drawGrid();
     this.loading = false;
+  }
+
+  drawGrid() {
+    this.columns = new Array(this.xDimension);
+    this.rows = new Array(this.yDimension)
   }
 
 }
